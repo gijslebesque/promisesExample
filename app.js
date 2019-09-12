d83b365d835969fec220a48177e3e4129085b925
gijslebesque <gijslebesque@gmail.com>
2019-09-12 17:17:19 +0200

    added async/await example

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ const updateUser = user => {
   });
 };
 
+const anotherUser = {
+  name: "Sam",
+  age: 28,
+  likes: ["Code"]
+};
+
 // <------ PROMISE EXECUTION ------>
 
 //here we call the our promises and chain them.
@@ -51,3 +57,20 @@ createUser(user)
   .then(returnValueOfResolveUpdateUserFunc => {
     console.log("Seconds user", returnValueOfResolveUpdateUserFunc);
   });
+
+// <------ ASYNC AWAIT------>
+
+//declare an asynchous function with async keyword
+async function aSynchronousFnc() {
+  //a try catch block for error handling
+  try {
+    //value of new user is the return value of the async func createUser.
+    const newUser = await createUser(anotherUser); //await means, the rest of the script won't be excecuted until method is resolved
+    console.log(newUser);
+    const updatedUser = await updateUser(newUser);
+    console.log(updatedUser);
+  } catch (err) {
+    console.log(err);
+  }
+}
+aSynchronousFnc();
